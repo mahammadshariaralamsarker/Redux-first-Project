551d6cbec046c76de0d69f01472df39ffe1d2e60
mahammadshariaralam <mahammadshariaralam@gmail.com>
2024-12-31 15:13:56 +0600

    increment and decrement

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import { useDispatch } from "react-redux";
+import { decreement, increment } from "./redux/feature/Counter/CounterSlice";
+
 function App() {
+  const dispatch = useDispatch();
+  const hadleIncrement = () => {
+    dispatch(increment())
+  };
+  const hadleDecrement = () => {
+    dispatch(decreement())
+  };
   return (
     <>
       <h1>This is First Redux Project</h1>
       <p>Counter With Redux</p>
       <div>
-        <button>Increement</button>
+        <button onClick={hadleIncrement}>Increement</button>
         <h5>Count:0</h5>
-        <button>Decrement</button>
+        <button onClick={hadleDecrement}>Decrement</button>
       </div>
     </>
   );
