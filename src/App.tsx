a53d3ca3b80ee6ebc3d3ec09be4d9df2cc8012ad
mahammadshariaralam <mahammadshariaralam@gmail.com>
2024-12-31 13:31:33 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <h1>This is First Redux Project</h1>
+      <p>Counter With Redux</p>
+      <div>
+        <button>Increement</button>
+        <h5>Count:0</h5>
+        <button>Decrement</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
