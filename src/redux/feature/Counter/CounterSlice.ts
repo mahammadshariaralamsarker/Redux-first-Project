aa112ef336ffc9666bde30564a4464dd60b4f57c
mahammadshariaralam <mahammadshariaralam@gmail.com>
2024-12-31 15:08:38 +0600

    dispatach

--- src/redux/feature/Counter/CounterSlice.ts
+++ src/redux/feature/Counter/CounterSlice.ts
@@ -17,6 +17,6 @@ const counterSlice = createSlice({
   },
 });
 
-export const {increment, decreement} = counterSlice.actions;
+export const { increment, decreement } = counterSlice.actions;
 
 export default counterSlice.reducer;
