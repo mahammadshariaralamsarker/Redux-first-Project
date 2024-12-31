c853352ca3638e3eb3c94bc1b5347af02aae9ac6
mahammadshariaralam <mahammadshariaralam@gmail.com>
2024-12-31 22:13:47 +0600

    brs

--- src/redux/hook.ts
+++ src/redux/hook.ts
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "./store";
 
-export const useAppSelector = useSelector.withTypes<RootState>()
+export const useAppSelector = useSelector.withTypes<RootState>();
 
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
