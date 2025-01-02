182594c7b2e5fcdf707df3a7b37917a597ed1b30
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-01-02 20:37:55 +0600

    addd react router dom

---
+++ src/routes/route.tsx
@@ -0,0 +1,10 @@
+import App from "@/App";
+import { createBrowserRouter } from "react-router-dom";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element:<App />
+  }
+])
+export default routes;
