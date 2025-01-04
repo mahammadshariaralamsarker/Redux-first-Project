dcf209ed6307a28da7f036bd6670caa4c41c9a51
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-01-04 12:06:14 +0600

    daa

--- src/redux/feature/Task/TaskSlice.tsx
+++ src/redux/feature/Task/TaskSlice.tsx
@@ -1,7 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  
+  task:{
+    id:"adhgaghf",
+    title:"Initialize Front-End",
+    descripton:"Create home page for and routing ",
+    dueDate:"2025-22",
+    inCompleted:false,
+    priority:"High"
+  }
 }
 
 
