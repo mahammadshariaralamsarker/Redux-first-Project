353ced2694dfcb581447e957dd8eeababf6b9459
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-01-04 10:55:56 +0600

    g

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
-import {store} from "./redux/store.ts";
+import { store } from "./redux/store.ts";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/route.tsx";
+import { ThemeProvider } from "./Providers/Theme-Provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    
-    <Provider store={store}>
-
-      <RouterProvider router={routes}/> 
-    </Provider>
+    <ThemeProvider>
+      <Provider store={store}>
+        <RouterProvider router={routes} />
+      </Provider>
+    </ThemeProvider>
   </StrictMode>
 );
