c67a7183e8946fc21bb6727a34e0cf011336fdba
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-01-04 12:26:37 +0600

    sadf

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CounterReducer from "./feature/Counter/CounterSlice";
-import  TaskReducer from './feature/Task/TaskSlice'
+import TaskReducer from "./feature/Task/TaskSlice";
 export const store = configureStore({
   reducer: {
     Counter: CounterReducer,
-    task:TaskReducer
+    task: TaskReducer,
   },
 });
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
